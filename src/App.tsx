3154d6b830a23b4ae9069367f7f19469e1d227d1
Zawadi <tamarazawadi101@gmail.com>
2026-09-21 12:54:54 +0300

    typewriter update

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ const App: React.FC = () => {
       let phraseIndex = 0;
       let charIndex = 0;
       let isDeleting = false;
-      let timeoutId: number;
 
       function type() {
         const currentPhrase = phrases[phraseIndex];
@@ -49,7 +48,7 @@ const App: React.FC = () => {
           typeSpeed = 500;
         }
 
-        timeoutId = setTimeout(type, typeSpeed);
+        setTimeout(type, typeSpeed);
       }
 
       type();
