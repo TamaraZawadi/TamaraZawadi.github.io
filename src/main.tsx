39db9b77b59dd8aa21483dd4296d6f2b6e66c398
Zawadi <tamarazawadi101@gmail.com>
2026-09-21 11:25:16 +0300

    custom

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import '../public/assets/js/custom.js'; // or adjust the path depending on where custom.js is located relative to your main file
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
